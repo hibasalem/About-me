467227eada26da7a108c1c996302102666217115
hibasalem <salemhiba.hs@gmail.com>
2021-03-30 17:53:13 +0300

    finished lab 03

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ this project is a practice prioject for lab 02 for 201 course
 
 # Author: hiba salem 
 
+How did this go, overall?
+this lab was exciting actually
+
+What observations or questions do you have about what you’ve learned so far?
+none
+
+How long did it take you to complete this assignment?
+about 3  hours
+
+And, before you started, how long did you think it would take you to complete this assignment?
+almost the same 
--- js/app.js
+++ js/app.js
@@ -8,19 +8,19 @@ let result=0
 
 //  -------------------------------------------1
 
-let dayPerson = prompt('Am I a day person?\n(answer with yes or no)')
+let dayPerson = prompt('Am I a day person?\n(answer with yes/y or no/n)')
 dayPerson=dayPerson.toLowerCase();
 
-while (dayPerson!=='no' && dayPerson!=='yes') {
-    alert('please answer with yes or no');
+while (dayPerson!=='no' && dayPerson!=='yes'&& dayPerson!=='y'&& dayPerson!=='n') {
+    alert('please answer with yes/y or no/n');
 }
 
-if (dayPerson==='yes') {
+if (dayPerson==='yes' || dayPerson==='y') {
     alert("noooo! i am the ultimate night owl");
-    //  console.log('your answer for\" Am I a day person?\" is false');
-}else if (dayPerson==='no'){
+     console.log('your answer for\" Am I a day person?\" is false');
+}else if (dayPerson==='no'|| dayPerson==='n'){
     alert("Righht!");
-    //  console.log('your answer for\" Am I a day person?\" is true');
+     console.log('your answer for\" Am I a day person?\" is true');
     result=result+1;
 }
 console.log(result);
@@ -29,16 +29,16 @@ console.log(result);
 let summerLover = prompt('Do I like summer?')
 summerLover=summerLover.toLowerCase();
 
-while (summerLover!=='no' && summerLover!=='yes') {
-    alert('please answer with yes or no');
+while (summerLover!=='no' && summerLover!=='yes'&& summerLover!=='y'&& summerLover!=='n') {
+    alert('please answer with yes/y or no/n');
 }
 
-if (summerLover==='yes') {
+if (summerLover==='yes'|| summerLover==='y') {
     alert("nope!");
-    // console.log('your answer for\" Do I like summer?\" is false');
-}else if (summerLover==='no'){
+    console.log('your answer for\" Do I like summer?\" is false');
+}else if (summerLover==='no'|| summerLover==='n'){
     alert("Right! winter is the best");
-    //  console.log('your answer for\" Do I like summer?\" is true');
+     console.log('your answer for\" Do I like summer?\" is true');
     result=result+1;
 }
 console.log(result);
@@ -48,17 +48,17 @@ console.log(result);
 let aliensBeliever = prompt('Do I believe in aliens?')
 aliensBeliever=aliensBeliever.toLowerCase();
 
-while (aliensBeliever!=='no' && aliensBeliever!=='yes') {
-    alert('please answer with yes or no');
+while (aliensBeliever!=='no' && aliensBeliever!=='yes' && aliensBeliever!=='n' && aliensBeliever!=='y' ) {
+    alert('please answer with yes/y or no/n');
 }
 
-if (aliensBeliever==='yes') {
+if (aliensBeliever==='yes'|| aliensBeliever==='y') {
     alert("sure i do ,why not!");
-    //  console.log('your answer for\" Do I believe in aliens?\" is true');
+     console.log('your answer for\" Do I believe in aliens?\" is true');
     result=result+1;
-}else if (aliensBeliever==='no'){
+}else if (aliensBeliever==='no'|| aliensBeliever==='y'){
     alert("no, i do believe on them actually!");
-    // console.log('your answer for\" Do I believe in aliens?\" is false');
+    console.log('your answer for\" Do I believe in aliens?\" is false');
 }
 
 console.log(result);
@@ -68,17 +68,17 @@ console.log(result);
 let introvertOrNot = prompt('Am I an introvert?')
 introvertOrNot=introvertOrNot.toLowerCase();
 
-while (introvertOrNot!=='no' && introvertOrNot!=='yes') {
-    alert('please answer with yes or no');
+while (introvertOrNot!=='no' && introvertOrNot!=='yes' && introvertOrNot!=='n' && introvertOrNot!=='y') {
+    alert('please answer with yes/y or no/n');
 }
 
-if (introvertOrNot==='yes') {
+if (introvertOrNot==='yes'|| introvertOrNot==='y') {
     alert("yes, that\'s true!");
-    // console.log('your answer for\" Am I an introvert?\" is true');
+    console.log('your answer for\" Am I an introvert?\" is true');
     result=result+1;
-}else if (introvertOrNot==='no'){
+}else if (introvertOrNot==='no'|| introvertOrNot==='y'){
     alert("no, i am an introvert!");
-    // console.log('your answer for\" Am I an introvert?\" is false');
+    console.log('your answer for\" Am I an introvert?\" is false');
 }
 console.log(result);
 
@@ -87,30 +87,76 @@ console.log(result);
 let coffeeLover = prompt('Am I a coffee lover?')
 coffeeLover=coffeeLover.toLowerCase();
 
-while (coffeeLover!=='no' && coffeeLover!=='yes') {
-    alert('please answer with yes or no');
+while (coffeeLover!=='no' && coffeeLover!=='yes' && coffeeLover!=='n' && coffeeLover!=='y') {
+    alert('please answer with yes/y or no/n');
 }
 
-if (coffeeLover==='yes') {
+if (coffeeLover==='yes'|| coffeeLover==='y') {
     alert("nope! tea all the time ");
-    // console.log('your answer for\" Am I a coffee lover?\" is false');
-}else if (coffeeLover==='no'){
+    console.log('your answer for\" Am I a coffee lover?\" is false');
+}else if (coffeeLover==='no'|| coffeeLover==='n'){
     alert("Right! tea all the time ");
-    // console.log('your answer for\" Am I a coffee lover?\" is true');
+    console.log('your answer for\" Am I a coffee lover?\" is true');
     result=result+1;
 }
 console.log(result);
 
+//  new ----------------------------------------6
+
+for(let i=4 ; i>0 ; i--){
+
+    let month =prompt('In witch month is my birthday? \nplease answer with a number\nyou got '+ i +' attempts') ;
+
+    if(month == 11){
+        alert("you got it right");
+        console.log('your answer for\"In witch month is my birthday?\" is true');
+        result=result+1;
+        break; 
+    }
+    else if (month == 12 ) {
+        alert("no , lower ");
+        console.log('your answer for\"In witch month is my birthday?\" is false');   
+
+    } else if ( month <11 && month >0) {
+        alert("no , higher ");
+        console.log('your answer for\"In witch month is my birthday?\" is false');   
+
+    }
+    else{
+        alert("please insert a number between 1 and 12");
+
+    }
+}
+
+//   -------------------------------------------7
+
+for (let ii = 6; ii > 0; ii--) {
+    let favColors = ['black', 'blue', 'red'];
+    console.log(favColors);
+
+    let userColor = prompt('what is my favoirte color?\nyou got ' + ii + ' attempts');
+    userColor = userColor.toLowerCase();
+
+    if (favColors[0] == userColor || favColors[1] == userColor || favColors[2] == userColor) {
+        alert('you got it right');
+        result=result+1;
+
+        break;
+    }else{
+        alert('try again');
+    }
+
+}
+alert("the correct answers is black , blue and red");
 
 //   -------------------------------------------
 
-console.log('your score is '+result+ ' out of 5');
+console.log('your score is '+result+ ' out of 7');
 
-if (result>=4) {
-alert(name+', your score is '+result+ ' out of 5 , you know me well!')     
+if (result>=6) {
+alert(name+', your score is '+result+ ' out of 7 , you know me well!')     
 }else{
-    alert(name+', your score is '+result+ ' out of 5 , we should get to know each other better!')
+    alert(name+', your score is '+result+ ' out of 7 , we should get to know each other better!')
 }
-
 
 document.getElementById('score').innerHTML = result;
