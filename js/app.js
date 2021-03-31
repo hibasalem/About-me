b0d91217000ba313c20aa8b89dbd6de3430cbcbe
hibasalem <salemhiba.hs@gmail.com>
2021-03-31 18:33:03 +0300

    edits

--- js/app.js
+++ js/app.js
@@ -1,32 +1,34 @@
 'use strict';
 
-let userName=prompt(' What is your name ?');
+let userName = prompt(' What is your name ?');
 
-alert('hello ' + userName+'! , hope you are having a great day.\nLets start with my about me game! ');
+alert('hello ' + userName + '! , hope you are having a great day.\nLets start with my about me game! ');
 
-let result=0;
+let result = 0;
 
 //  -------------------------------------------1
 
 let dayPerson = prompt('Am I a day person?\n(answer with yes/y or no/n)');
+dayPerson = dayPerson.toLowerCase();
 
-function qes(dayPerson){
-  dayPerson=dayPerson.toLowerCase();
+function qes(dayPerson) {
 
-  while (dayPerson!=='no' && dayPerson!=='yes'&& dayPerson!=='y'&& dayPerson!=='n') {
+  while (dayPerson !== 'no' && dayPerson !== 'yes' && dayPerson !== 'y' && dayPerson !== 'n') {
     alert('please answer with yes/y or no/n');
+    dayPerson = prompt('Am I a day person?\n(answer with yes/y or no/n)');
+    dayPerson = dayPerson.toLowerCase();
   }
 
-  if (dayPerson==='yes' || dayPerson==='y') {
+  if (dayPerson === 'yes' || dayPerson === 'y') {
     alert('noooo! i am the ultimate night owl');
     console.log('your answer for\" Am I a day person?\" is false');
-  }else if (dayPerson==='no'|| dayPerson==='n'){
+  } else if (dayPerson === 'no' || dayPerson === 'n') {
     alert('Righht!');
     console.log('your answer for\" Am I a day person?\" is true');
-    result=result+1;
+    result = result + 1;
   }
   console.log(result);
-  return(result);
+  return (result);
 }
 
 qes(dayPerson);
@@ -35,145 +37,157 @@ qes(dayPerson);
 
 
 let summerLover = prompt('Do I like summer?');
+summerLover = summerLover.toLowerCase();
 
-function qes2(summerLover){
-  summerLover=summerLover.toLowerCase();
+function qes2(summerLover) {
 
-  while (summerLover!=='no' && summerLover!=='yes'&& summerLover!=='y'&& summerLover!=='n') {
+
+  while (summerLover !== 'no' && summerLover !== 'yes' && summerLover !== 'y' && summerLover !== 'n') {
     alert('please answer with yes/y or no/n');
+    summerLover = prompt('Do I like summer?');
+    summerLover = summerLover.toLowerCase();
   }
 
-  if (summerLover==='yes'|| summerLover==='y') {
+  if (summerLover === 'yes' || summerLover === 'y') {
     alert('nope!');
     console.log('your answer for\" Do I like summer?\" is false');
-  }else if (summerLover==='no'|| summerLover==='n'){
+  } else if (summerLover === 'no' || summerLover === 'n') {
     alert('Right! winter is the best');
     console.log('your answer for\" Do I like summer?\" is true');
-    result=result+1;
+    result = result + 1;
   }
   console.log(result);
-  return(result);
+  return (result);
 }
 qes2(summerLover);
 //  -------------------------------------------3
 
 
 let aliensBeliever = prompt('Do I believe in aliens?');
+aliensBeliever = aliensBeliever.toLowerCase();
 
-function qes3(aliensBeliever){
-  aliensBeliever=aliensBeliever.toLowerCase();
+function qes3(aliensBeliever) {
 
-  while (aliensBeliever!=='no' && aliensBeliever!=='yes' && aliensBeliever!=='n' && aliensBeliever!=='y' ) {
+
+  while (aliensBeliever !== 'no' && aliensBeliever !== 'yes' && aliensBeliever !== 'n' && aliensBeliever !== 'y') {
     alert('please answer with yes/y or no/n');
+    aliensBeliever = prompt('Do I believe in aliens?');
+    aliensBeliever = aliensBeliever.toLowerCase();
   }
 
-  if (aliensBeliever==='yes'|| aliensBeliever==='y') {
+  if (aliensBeliever === 'yes' || aliensBeliever === 'y') {
     alert('sure i do ,why not!');
     console.log('your answer for\" Do I believe in aliens?\" is true');
-    result=result+1;
-  }else if (aliensBeliever==='no'|| aliensBeliever==='y'){
+    result = result + 1;
+  } else if (aliensBeliever === 'no' || aliensBeliever === 'y') {
     alert('no, i do believe on them actually!');
     console.log('your answer for\" Do I believe in aliens?\" is false');
   }
 
   console.log(result);
-  return(result);
+  return (result);
 }
 qes3(aliensBeliever);
 //  -------------------------------------------4
 
 
 let introvertOrNot = prompt('Am I an introvert?');
+introvertOrNot = introvertOrNot.toLowerCase();
 
-function qes4(introvertOrNot){
-  introvertOrNot=introvertOrNot.toLowerCase();
+function qes4(introvertOrNot) {
 
-  while (introvertOrNot!=='no' && introvertOrNot!=='yes' && introvertOrNot!=='n' && introvertOrNot!=='y') {
+  while (introvertOrNot !== 'no' && introvertOrNot !== 'yes' && introvertOrNot !== 'n' && introvertOrNot !== 'y') {
     alert('please answer with yes/y or no/n');
+    introvertOrNot = prompt('Am I an introvert?');
+    introvertOrNot = introvertOrNot.toLowerCase();
   }
 
-  if (introvertOrNot==='yes'|| introvertOrNot==='y') {
+  if (introvertOrNot === 'yes' || introvertOrNot === 'y') {
     alert('yes, that\'s true!');
     console.log('your answer for\" Am I an introvert?\" is true');
-    result=result+1;
-  }else if (introvertOrNot==='no'|| introvertOrNot==='y'){
+    result = result + 1;
+  } else if (introvertOrNot === 'no' || introvertOrNot === 'y') {
     alert('no, i am an introvert!');
     console.log('your answer for\" Am I an introvert?\" is false');
   }
   console.log(result);
-  return(result);
+  return (result);
 }
 qes4(introvertOrNot);
 //  -------------------------------------------5
 
 
 let coffeeLover = prompt('Am I a coffee lover?');
+coffeeLover = coffeeLover.toLowerCase();
 
-function qes5(coffeeLover){
-  coffeeLover=coffeeLover.toLowerCase();
 
-  while (coffeeLover!=='no' && coffeeLover!=='yes' && coffeeLover!=='n' && coffeeLover!=='y') {
+function qes5(coffeeLover) {
+
+  while (coffeeLover !== 'no' && coffeeLover !== 'yes' && coffeeLover !== 'n' && coffeeLover !== 'y') {
     alert('please answer with yes/y or no/n');
+    coffeeLover = prompt('Am I a coffee lover?');
+    coffeeLover = coffeeLover.toLowerCase();
   }
 
-  if (coffeeLover==='yes'|| coffeeLover==='y') {
+  if (coffeeLover === 'yes' || coffeeLover === 'y') {
     alert('nope! tea all the time ');
     console.log('your answer for\" Am I a coffee lover?\" is false');
-  }else if (coffeeLover==='no'|| coffeeLover==='n'){
+  } else if (coffeeLover === 'no' || coffeeLover === 'n') {
     alert('Right! tea all the time ');
     console.log('your answer for\" Am I a coffee lover?\" is true');
-    result=result+1;
+    result = result + 1;
   }
   console.log(result);
-  return(result);
+  return (result);
 }
 qes5(coffeeLover);
+
 //  new ----------------------------------------6
 
+let month = 0;
+
+function qes6() {
 
 
-function qes6(){
+  for (let i = 4; i > 0; i--) {
 
-  let month = 0;
+    let month = prompt('In witch month is my birthday? \nplease answer with a number\nyou got ' + i + ' attempts');
+    month = parseInt(month);
 
-  for(let i=4 ; i>0 ; i--){
-
-    let month =prompt('In witch month is my birthday? \nplease answer with a number\nyou got '+ i +' attempts') ;
-    month=parseInt(month);
-
-    if(month === 11){
+    if (month === 11) {
       alert('you got it right');
       console.log('your answer for\"In witch month is my birthday?\" is true');
-      result=result+1;
+      result = result + 1;
       break;
     }
-    else if (month === 12 ) {
+    else if (month === 12) {
       alert('no , lower ');
       console.log('your answer for\"In witch month is my birthday?\" is false');
 
-    } else if ( month <11 && month >0) {
+    } else if (month < 11 && month > 0) {
       alert('no , higher ');
       console.log('your answer for\"In witch month is my birthday?\" is false');
     }
-    else{
+    else {
       alert('please insert a number between 1 and 12');
 
     }
-    if(i===1){
+    if (i === 1) {
       alert('the correct answer is 11 ');
     }
   }
-  return(result);
+  return (result);
 }
 qes6();
+
 //   -------------------------------------------7
 
-let favColors =0;
-let userColor =0;
+let favColors = 0;
+let userColor = 0;
 
-function qes7(){
+function qes7() {
 
-  for (let j = 6; j> 0; j--) {
+  for (let j = 6; j > 0; j--) {
     let favColors = ['black', 'blue', 'red'];
     console.log(favColors);
 
@@ -183,29 +197,29 @@ function qes7(){
     if (favColors[0] === userColor || favColors[1] === userColor || favColors[2] === userColor) {
       alert('you got it right');
       alert('the correct answers is black , blue and red ');
-      result=result+1;
+      result = result + 1;
 
       break;
-    }else{
+    } else {
       alert('try again');
     }
-    if(j===1){
+    if (j === 1) {
       alert('the correct answers is black , blue and red ');
     }
 
   }
-  return(result);
+  return (result);
 }
 
 qes7();
 //   -------------------------------------------
 
-console.log('your score is '+result+ ' out of 7');
+console.log('your score is ' + result + ' out of 7');
 
-if (result>=6) {
-  alert(userName+', your score is '+result+ ' out of 7 , you know me well!');
-}else{
-  alert(userName+', your score is '+result+ ' out of 7 , we should get to know each other better!');
+if (result >= 6) {
+  alert(userName + ', your score is ' + result + ' out of 7 , you know me well!');
+} else {
+  alert(userName + ', your score is ' + result + ' out of 7 , we should get to know each other better!');
 }
 
 document.getElementById('score').innerHTML = result;
