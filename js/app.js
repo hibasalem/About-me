d9f368defe607c22655860ffc92e097668f77452
hibasalem <salemhiba.hs@gmail.com>
2021-04-04 03:20:24 +0300

    finished the project

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ let result = 0;
 let dayPerson = prompt('Am I a day person?\n(answer with yes/y or no/n)');
 dayPerson = dayPerson.toLowerCase();
 
-function qes(dayPerson) {
+function qes1(dayPerson) {
 
   while (dayPerson !== 'no' && dayPerson !== 'yes' && dayPerson !== 'y' && dayPerson !== 'n') {
     alert('please answer with yes/y or no/n');
@@ -21,19 +21,14 @@ function qes(dayPerson) {
 
   if (dayPerson === 'yes' || dayPerson === 'y') {
     alert('noooo! i am the ultimate night owl');
-    console.log('your answer for\" Am I a day person?\" is false');
   } else if (dayPerson === 'no' || dayPerson === 'n') {
     alert('Righht!');
-    console.log('your answer for\" Am I a day person?\" is true');
     result = result + 1;
   }
-  console.log(result);
   return (result);
 }
 
-qes(dayPerson);
-
-//  -------------------------------------------2
+qes1(dayPerson);
 
 
 let summerLover = prompt('Do I like summer?');
@@ -50,17 +45,14 @@ function qes2(summerLover) {
 
   if (summerLover === 'yes' || summerLover === 'y') {
     alert('nope!');
-    console.log('your answer for\" Do I like summer?\" is false');
   } else if (summerLover === 'no' || summerLover === 'n') {
     alert('Right! winter is the best');
-    console.log('your answer for\" Do I like summer?\" is true');
     result = result + 1;
   }
-  console.log(result);
   return (result);
 }
 qes2(summerLover);
-//  -------------------------------------------3
+
 
 
 let aliensBeliever = prompt('Do I believe in aliens?');
@@ -77,18 +69,13 @@ function qes3(aliensBeliever) {
 
   if (aliensBeliever === 'yes' || aliensBeliever === 'y') {
     alert('sure i do ,why not!');
-    console.log('your answer for\" Do I believe in aliens?\" is true');
     result = result + 1;
   } else if (aliensBeliever === 'no' || aliensBeliever === 'y') {
     alert('no, i do believe on them actually!');
-    console.log('your answer for\" Do I believe in aliens?\" is false');
   }
-
-  console.log(result);
   return (result);
 }
 qes3(aliensBeliever);
-//  -------------------------------------------4
 
 
 let introvertOrNot = prompt('Am I an introvert?');
@@ -104,22 +91,17 @@ function qes4(introvertOrNot) {
 
   if (introvertOrNot === 'yes' || introvertOrNot === 'y') {
     alert('yes, that\'s true!');
-    console.log('your answer for\" Am I an introvert?\" is true');
     result = result + 1;
   } else if (introvertOrNot === 'no' || introvertOrNot === 'y') {
     alert('no, i am an introvert!');
-    console.log('your answer for\" Am I an introvert?\" is false');
   }
-  console.log(result);
   return (result);
 }
 qes4(introvertOrNot);
-//  -------------------------------------------5
 
 
 let coffeeLover = prompt('Am I a coffee lover?');
 coffeeLover = coffeeLover.toLowerCase();
-
 
 function qes5(coffeeLover) {
 
@@ -131,23 +113,17 @@ function qes5(coffeeLover) {
 
   if (coffeeLover === 'yes' || coffeeLover === 'y') {
     alert('nope! tea all the time ');
-    console.log('your answer for\" Am I a coffee lover?\" is false');
   } else if (coffeeLover === 'no' || coffeeLover === 'n') {
     alert('Right! tea all the time ');
-    console.log('your answer for\" Am I a coffee lover?\" is true');
     result = result + 1;
   }
-  console.log(result);
   return (result);
 }
 qes5(coffeeLover);
 
-//  new ----------------------------------------6
 
 let month = 0;
-
 function qes6() {
-
 
   for (let i = 4; i > 0; i--) {
 
@@ -156,17 +132,14 @@ function qes6() {
 
     if (month === 11) {
       alert('you got it right');
-      console.log('your answer for\"In witch month is my birthday?\" is true');
       result = result + 1;
       break;
     }
     else if (month === 12) {
       alert('no , lower ');
-      console.log('your answer for\"In witch month is my birthday?\" is false');
 
     } else if (month < 11 && month > 0) {
       alert('no , higher ');
-      console.log('your answer for\"In witch month is my birthday?\" is false');
     }
     else {
       alert('please insert a number between 1 and 12');
@@ -180,15 +153,13 @@ function qes6() {
 }
 qes6();
 
-//   -------------------------------------------7
 
 let favColors = 0;
 let userColor = 0;
-
 function qes7() {
 
   for (let j = 6; j > 0; j--) {
-    let favColors = ['black', 'blue', 'red'];
+    let favColors = ['black', 'white', 'red'];
     console.log(favColors);
 
     let userColor = prompt('what is my favoirte color?\nyou got ' + j + ' attempts');
@@ -196,30 +167,27 @@ function qes7() {
 
     if (favColors[0] === userColor || favColors[1] === userColor || favColors[2] === userColor) {
       alert('you got it right');
-      alert('the correct answers is black , blue and red ');
+      alert('the correct answers is black , white and red ');
       result = result + 1;
-
       break;
+
     } else {
       alert('try again');
     }
+
     if (j === 1) {
-      alert('the correct answers is black , blue and red ');
+      alert('the correct answers is black , white and red ');
     }
 
   }
   return (result);
 }
-
 qes7();
-//   -------------------------------------------
 
-console.log('your score is ' + result + ' out of 7');
 
 if (result >= 6) {
   alert(userName + ', your score is ' + result + ' out of 7 , you know me well!');
 } else {
   alert(userName + ', your score is ' + result + ' out of 7 , we should get to know each other better!');
 }
-
 document.getElementById('score').innerHTML = result;
